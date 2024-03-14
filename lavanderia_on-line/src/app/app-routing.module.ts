a26e6a7f5ea283971a9620e01d168c2515a5c315
Victor <victorgabrielhilario28@gmail.com>
2024-03-14 16:47:54 -0300

    Cadastro Cliente Html

--- lavanderia_on-line/src/app/app-routing.module.ts
+++ lavanderia_on-line/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutenticacaoComponent } from './autenticacao/autenticacao.component'
+import { CadastroClienteComponent } from './cliente/cadastro-cliente/cadastro-cliente.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/autenticacao', pathMatch: 'full' },
   { path: 'autenticacao', component: AutenticacaoComponent },
+  { path: 'cadastro', component: CadastroClienteComponent},
 ];
 
 @NgModule({
--- lavanderia_on-line/src/app/app.module.ts
+++ lavanderia_on-line/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CadastroClienteComponent } from './cliente/cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
