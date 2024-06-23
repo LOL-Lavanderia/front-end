95a23c025ed6d21731cfdb7aeb8b5f9a2a7e0c9a
maidell <mateusmaidel@gmail.com>
2024-06-23 20:16:24 -0300

    :white_check_mark: Torna o model usuario mais completo usando tipo pra checar a role

--- lavanderia_on-line/src/app/shared/models/usuario/usuario.ts
+++ lavanderia_on-line/src/app/shared/models/usuario/usuario.ts
@@ -1,7 +1,39 @@
+export type Client = { role: "client", cpf: string, address: Address[], phone: Phone[]};
+export type Employee = { role: "employee", birthDate: string };
+export type Role = Client | Employee;
+
+export function isEmployee(user: Usuario): boolean {
+  return user.role.role === "employee";
+}
+
 export class Usuario {
-    constructor(
-      public email: string,
-      public password: string
-    ) {}
-  }
-  
+  constructor(
+    public id: string | undefined,
+    public email: string,
+    public name: string,
+    public password: string,
+    public role: Role,
+  ) { }
+}
+
+export interface Address {
+  id: number;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  cep: number;
+  tipo: number;
+}
+
+export interface Phone {
+  id: number;
+  numero: number;
+}
+
+export class Auth {
+  constructor(
+    public email?: string,
+    public password?: string,
+  ) { }
+}
