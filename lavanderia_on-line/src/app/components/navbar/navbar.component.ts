69b52c67509ddaf2f05e3dbc03e5af68316c7238
Victor <victorgabrielhilario28@gmail.com>
2024-07-26 16:30:07 -0300

    Navbar por perfil

--- lavanderia_on-line/src/app/components/navbar/navbar.component.ts
+++ lavanderia_on-line/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../shared/services/authenticationservice/authentication.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  isEmployee: boolean = false;
+export class NavbarComponent implements OnInit {
+  isEmployee: boolean = true;
 
   constructor(private authService: AuthenticationService) { }
+  ngOnInit(): void {
+    if(this.authService.usuarioLogado.role.role === "client"){
+      this.isEmployee = false;
+    };
+  }
 
   logout(): void {
     this.authService.logout();
